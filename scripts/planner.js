a4b96fe7aa0e8fb0efed7e79b753169af91978d1
dameoffensive-cloud <dameoffensive@gmail.com>
2026-02-08 12:03:57 -0500

    Add files via upload

--- scripts/planner.js
+++ scripts/planner.js
@@ -201,6 +201,11 @@ function planner_controller($scope){
 				update(self.years[0].data.greenhouse, true); // Update greenhouse
 				
 				self.loaded = true;
+		// Footer: show data update date
+		if (data.updated_at){
+			$("#footer_version").text("Planner data last updated: " + data.updated_at);
+		}
+
 				$scope.$apply();
 			},
 			error: function(xhr, status, error){
